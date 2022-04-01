c76d973085c8f1a0e345300cc27567398eee1b84
Ukaoha Chizoba <ukaohachizoba6@gmail.com>
2022-04-01 13:59:51 +0100

    made some changes

--- script.js
+++ script.js
@@ -118,6 +118,26 @@ window.addEventListener("scroll", function () {
   else nav.classList.remove("sticky");
 });
 */
+// navigate media quereies 
+const hamburger = document.querySelector('.hamburger');
+const navItem = document.querySelector('.nav__links')
+
+hamburger.addEventListener('click', mobileMenu );
+
+function mobileMenu(){
+  hamburger.classList.toggle('active');
+  navItem.classList.toggle('active');
+}
+
+const navLink = document.querySelectorAll('.nav__link');
+navLink.forEach(n=> n.addEventListener('click', closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove('active');
+  navItem.classList.remove('active')
+}
+
+
 // Tabbed component
 const tabs = document.querySelectorAll(".operations__tab");
 const tabContainer = document.querySelector(".operations__tab-container");
